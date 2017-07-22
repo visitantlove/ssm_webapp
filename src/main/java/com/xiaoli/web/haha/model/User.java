b2c92d9214b783312ff35d6ca4dfaf7a6da97a6d
zhangli <xiaoli9233@163.com>
2017-07-22 12:13:21 +0800

    mybatis 逆向工程

---
+++ src/main/java/com/xiaoli/web/haha/model/User.java
@@ -0,0 +1,111 @@
+package com.xiaoli.web.haha.model;
+
+import java.util.Date;
+
+public class User {
+    private Long id;
+
+    private String userName;
+
+    private String userPhone;
+
+    private String userEmail;
+
+    private String userPwd;
+
+    private String pwdSalt;
+
+    private Date createTime;
+
+    private Date modifyTime;
+
+    private Byte isDelete;
+
+    public User(Long id, String userName, String userPhone, String userEmail, String userPwd, String pwdSalt, Date createTime, Date modifyTime, Byte isDelete) {
+        this.id = id;
+        this.userName = userName;
+        this.userPhone = userPhone;
+        this.userEmail = userEmail;
+        this.userPwd = userPwd;
+        this.pwdSalt = pwdSalt;
+        this.createTime = createTime;
+        this.modifyTime = modifyTime;
+        this.isDelete = isDelete;
+    }
+
+    public User() {
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone == null ? null : userPhone.trim();
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail == null ? null : userEmail.trim();
+    }
+
+    public String getUserPwd() {
+        return userPwd;
+    }
+
+    public void setUserPwd(String userPwd) {
+        this.userPwd = userPwd == null ? null : userPwd.trim();
+    }
+
+    public String getPwdSalt() {
+        return pwdSalt;
+    }
+
+    public void setPwdSalt(String pwdSalt) {
+        this.pwdSalt = pwdSalt == null ? null : pwdSalt.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getModifyTime() {
+        return modifyTime;
+    }
+
+    public void setModifyTime(Date modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+
+    public Byte getIsDelete() {
+        return isDelete;
+    }
+
+    public void setIsDelete(Byte isDelete) {
+        this.isDelete = isDelete;
+    }
+}
